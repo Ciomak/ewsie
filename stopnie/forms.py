0c7d8135d69538706c458c5b1975a134bfa19e9a
Kamil Ciomcia <kamil.ciomcia@orange.com>
2016-08-03 15:39:39 +0200

    Fix choice field kierunek from model Kierunek. Add migrations to .gitignore

--- stopnie/forms.py
+++ stopnie/forms.py
@@ -2,17 +2,17 @@
 
 from django import forms
 
-from models import Kierunek
+from stopnie.models import Kierunek
 
 
 class RejestracjaForm(forms.Form):
 	imie = forms.CharField(max_length=150, label='Imię')
 	nazwisko = forms.CharField(max_length=150, label='Nazwisko')
-	kierunek = forms.ChoiceField()
+	kierunek = forms.ChoiceField(choices=[(k.id, k.name) for k in Kierunek.objects.all()])
 	lokalizacja = forms.CharField(max_length=150)
 	mail = forms.EmailField(label='E-mail')
 	telefon = forms.CharField(max_length=20, label='Telefon')
-	zgoda = forms.BooleanField(label='Wyrażam zgodę na przeprowadzenie ankiety')
+	zgoda = forms.BooleanField(label='Wyrażam zgodę na przeprowadzenie ankiety', required=False)
 	
 	
 		
